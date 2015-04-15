74fb5ce91a863973316eac080b7be82e2b6153aa
Simo Sorce <simo@redhat.com>
2015-04-15 12:10:22 -0400

    Update setup.py
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- setup.py
+++ setup.py
@@ -6,12 +6,20 @@ from distutils.core import setup
 
 setup(
     name = 'jwcrypto',
-    version = '0.0.1',
+    version = '0.1.0',
     license = 'LGPLv3+',
     maintainer = 'JWCrypto Project Contributors',
     maintainer_email = 'simo@redhat.com',
     url='https://github.com/simo5/jwcrypto',
     packages = ['jwcrypto'],
+    description = 'Implementation of JOSE Web standards',
+    classifiers = [
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Intended Audience :: Developers',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
     data_files = [('share/doc/jwcrypto', ['LICENSE', 'README.md'])],
 )
 
