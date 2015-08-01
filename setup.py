8352a0578065950994e0edbeccf6b687f511498a
Simo Sorce <simo@redhat.com>
2015-08-01 14:53:04 -0400

    Raise version to 0.2.1
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'jwcrypto',
-    version = '0.2.0',
+    version = '0.2.1',
     license = 'LGPLv3+',
     maintainer = 'JWCrypto Project Contributors',
     maintainer_email = 'simo@redhat.com',
