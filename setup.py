364121b41f3c1571d74c16cc1e465dc72f4d668d
Simo Sorce <simo@redhat.com>
2016-08-19 10:25:33 -0400

    Update docs and project URL
    Version is now at 0.3.0, docs were still sayion 0.1.0.
    Also update setup.py to the correct URL for the project.
    Change classifiers and other code to properly test with 3.4 and 3.5,
    drop 3.3 as it is untested at this point.
    
    Signed-off-by: Simo Sorce <simo@redhat.com>
    Reviewed-by: Christian Heimes <cheimes@redhat.com>
    Closes #51

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ setup(
     license = 'LGPLv3+',
     maintainer = 'JWCrypto Project Contributors',
     maintainer_email = 'simo@redhat.com',
-    url='https://github.com/simo5/jwcrypto',
+    url='https://github.com/latchset/jwcrypto',
     packages = ['jwcrypto'],
     description = 'Implementation of JOSE Web standards',
     classifiers = [
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Intended Audience :: Developers',
         'Topic :: Security',
         'Topic :: Software Development :: Libraries :: Python Modules'
