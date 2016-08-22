e0b3d9d2a109890130bc60d155a42a6b1a6b7bb4
Simo Sorce <simo@redhat.com>
2016-08-22 17:12:10 -0400

    Allow to pass a default reciepient to a JWE
    Resolves #53
    Signed-off-by: Simo Sorce <simo@redhat.com>
    Reviewed-by: Hanno Schlichting <hanno@hannosch.eu>
    Closes #56

--- jwcrypto/tests.py
+++ jwcrypto/tests.py
@@ -907,3 +907,12 @@ class ConformanceTests(unittest.TestCase):
         self.assertEqual(check.payload, b'message')
         self.assertEqual(
             json_decode(check.objects['header'])['kid'], key.thumbprint())
+
+    def test_jwe_default_recipient(self):
+        key = jwk.JWK(kty='oct', k=base64url_encode(b'A' * (128 // 8)))
+        enc = jwe.JWE(plaintext='plain',
+                      protected='{"alg":"A128KW","enc":"A128GCM"}',
+                      recipient=key).serialize()
+        check = jwe.JWE()
+        check.deserialize(enc, key)
+        self.assertEqual(b'plain', check.payload)
