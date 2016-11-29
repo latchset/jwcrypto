b9949a7987f5df983ba361e67fd6be0813cdecde
Christian Heimes <cheimes@redhat.com>
2016-11-29 20:50:28 +0100

    Post release bump
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'jwcrypto',
-    version = '0.4.0',
+    version = '0.4.0.dev1',
     license = 'LGPLv3+',
     maintainer = 'JWCrypto Project Contributors',
     maintainer_email = 'simo@redhat.com',
