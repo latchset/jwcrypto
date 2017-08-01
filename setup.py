c4637c14ce129bc09dfdf04e2f794f65ff7340b2
Christian Heimes <cheimes@redhat.com>
2017-08-01 17:56:23 +0200

    Preparing release 0.4.2
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'jwcrypto',
-    version = '0.5.dev1',
+    version = '0.4.2',
     license = 'LGPLv3+',
     maintainer = 'JWCrypto Project Contributors',
     maintainer_email = 'simo@redhat.com',
