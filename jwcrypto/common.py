4f2c45b785feffcb04d085e6c20d588d9394a49c
Jeff Schwaber <jschwaber@mobilepaks.com>
2018-05-26 00:58:28 -0700

    if someone generates a jwe token in Ruby, using Ruby's JOSE library,     they can use the bytestring digest as a key. This code allows     the jwcrypto library to decode that jwe token.

--- jwcrypto/common.py
+++ jwcrypto/common.py
@@ -23,7 +23,12 @@ def base64url_decode(payload):
         payload += '='
     elif size != 0:
         raise ValueError('Invalid base64 string')
-    return urlsafe_b64decode(payload.encode('utf-8'))
+    try:
+        return urlsafe_b64decode(payload.encode('utf'))
+    except (UnicodeDecodeError):
+        return payload
+
+
 
 
 # JSON encoding/decoding helpers with good defaults
