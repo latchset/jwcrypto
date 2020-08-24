f0d93764ea5ed09d7c8bf6d699448b8efd7567f8
Simo Sorce <simo@redhat.com>
2020-08-24 14:51:12 -0400

    Post release bump to 0.9.dev1
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'jwcrypto',
-    version = '0.8',
+    version = '0.9.dev1',
     license = 'LGPLv3+',
     maintainer = 'JWCrypto Project Contributors',
     maintainer_email = 'simo@redhat.com',
