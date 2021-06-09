1ccdaec0cb9783cc66eef90e6e3d2ce471a0199e
Simo Sorce <simo@redhat.com>
2021-06-09 14:30:35 -0400

    Release 0.9.1
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'jwcrypto',
-    version = '0.9',
+    version = '0.9.1',
     license = 'LGPLv3+',
     maintainer = 'JWCrypto Project Contributors',
     maintainer_email = 'simo@redhat.com',
