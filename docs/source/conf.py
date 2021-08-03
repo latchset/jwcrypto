31240bc86bc268691bf457354222dbcc5da084c9
DefteZ <deftez@gmail.com>
2021-08-03 13:40:46 +0200

    Update copyrights in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'JWCrypto'
-copyright = u'2016-2018, JWCrypto Contributors'
+copyright = u'2016-2021, JWCrypto Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
